f46fc74eb0f11f59f60f946741ef810347fc0cbb
mbarros46 <miguelbarrosramos46@gmail.com>
2024-06-03 09:39:19 -0300

    pagina vendas js feita

---
+++ javascript/pagina-vendas.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    const tipoLixo = document.getElementById('tipoLixo').value;
+    const quantidadeLixo = document.getElementById('quantidadeLixo').value;
+    alert(`Tipo de Lixo: ${tipoLixo}\nQuantidade: ${quantidadeLixo} gramas`);
+}
