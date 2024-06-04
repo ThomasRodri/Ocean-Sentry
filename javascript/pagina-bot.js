8a180993ac64fca9eaba3fd8d28c890391e30d62
mbarros46 <miguelbarrosramos46@gmail.com>
2024-06-04 09:22:55 -0300

    pagina do bot feita

---
+++ javascript/pagina-bot.js
@@ -0,0 +1,7 @@
+
+//  um script básico para demonstrar a estrutura do arquivo.
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Página carregada e script.js em funcionamento');
+});
